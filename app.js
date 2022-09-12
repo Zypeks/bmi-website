0b6f3360f2f78f211a7aa68a6d76b6feb453a814
adis0n <112665233+Zypeks@users.noreply.github.com>
2022-09-12 21:34:01 +0200

    Add files via upload

--- app.js
+++ app.js
@@ -1,5 +1,19 @@
-
-function BMI() {
+window.onload = function()
+{
+        
+        document.getElementById('bmi-menu').addEventListener('click',function()
+                {
+                        document.getElementById('button').setAttribute( "onClick", "functionBMI()" );
+                        document.getElementById('title').textContent= 'BMI Calculator'
+                });
+        document.getElementById('bmr-menu').addEventListener('click',function()
+                {
+                        document.getElementById('button').setAttribute( "onClick", "functionBMR()" );
+                        document.getElementById('title').textContent= 'BMR Calculator'
+                });
+                
+}
+function functionBMI() {
         let height = document.getElementById('height').value;
         let weight = document.getElementById('weight').value;
         let BMI = (weight/(height/100*height/100)).toFixed(1);
@@ -10,24 +24,23 @@ function BMI() {
         if (BMI >= 18.5 && BMI <= 25) document.getElementById("comment").innerHTML = "Your weight is Normal";
         if (BMI >= 25 && BMI <= 30) document.getElementById("comment").innerHTML = "You got Obese";
         if (BMI > 30) document.getElementById("comment").innerHTML = "You got Overweight" ;
-        document.getElementById('diet').innerHTML = "Check out our diets for : " + kcal + " kcal" ;
-        
+        document.getElementById('diet').innerHTML = "Check out our diets for : " + kcal + " kcal" ;        
 }
+function functionBMR() {
+        let age = document.getElementById('age').value;         
+        let height = document.getElementById('height').value;
+        let weight = document.getElementById('weight').value;
+        let BMR = (88.362 + (13.397*weight) + (4.799 * height) - (5.677*age)).toFixed(1);
+        if (document.getElementById('female').checked)
+        {
+                let BMRwomen = (447.593 + (9.247*weight) + (3.098*height)-(4.330*age)).toFixed(1);
+                document.getElementById("output").innerHTML ='Your The basal metabolic rate is: ' + BMRwomen;
+                document.getElementById('diet').innerHTML = "Check out our diets for : " + BMRwomen + " kcal" ;
+        }
+        else{
 
-window.onload = function()
-{
         
-        document.getElementById('bmi-menu').addEventListener('click',function()
-                {
-                        document.getElementById('button').setAttribute( "onClick", "BMI()" );
-                        document.getElementById('title').textContent= 'BMI Calculator'
-                });
-        document.getElementById('bmr-menu').addEventListener('click',function()
-                {
-                        document.getElementById('button').setAttribute( "onClick", "BMR()" );
-                        document.getElementById('title').textContent= 'BMR Calculator'
-                });
-                
-
+        document.getElementById("output").innerHTML ='Your The basal metabolic rate is: ' + BMR;
+        document.getElementById('diet').innerHTML = "Check out our diets for : " + BMR + " kcal" ;
+        }
 }
-
